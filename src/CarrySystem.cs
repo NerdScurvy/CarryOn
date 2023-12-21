e29ca001c3a8969e8d64aaef4a9c2729ec1276d3
NerdScurvy <NerdScurvy@gmail.com>
2023-12-21 10:18:07 +0800

    Bumped version to target pre.10

--- src/CarrySystem.cs
+++ src/CarrySystem.cs
@@ -15,11 +15,11 @@ using Vintagestory.API.Util;
 
 [assembly: ModInfo("Carry On", 
     modID: "carryon",
-    Version = "1.7.0-pre.2",
+    Version = "1.7.0-pre.3",
     Description = "Adds the capability to carry various things",
     Website = "https://github.com/NerdScurvy/CarryOn",
     Authors = new[] { "copygirl", "NerdScurvy" })]
-[assembly: ModDependency("game", "1.19.0-pre.7")]
+[assembly: ModDependency("game", "1.19.0-pre.10")]
 
 namespace CarryOn
 {
