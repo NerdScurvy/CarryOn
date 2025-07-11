73931d82965905e2bb4663c6b11c2d3afc27ff05
NerdScurvy <NerdScurvy@gmail.com>
2025-07-11 22:49:16 +0800

    Target .Net 8.0

--- src/CarrySystem.cs
+++ src/CarrySystem.cs
@@ -15,11 +15,11 @@ using Vintagestory.API.Util;
 
 [assembly: ModInfo("Carry On", 
     modID: "carryon",
-    Version = "1.8.1",
+    Version = "1.10.0-pre.1",
     Description = "Adds the capability to carry various things",
     Website = "https://github.com/NerdScurvy/CarryOn",
     Authors = new[] { "copygirl", "NerdScurvy" })]
-[assembly: ModDependency("game", "1.20.4")]
+[assembly: ModDependency("game", "1.21.0-pre.2")]
 
 namespace CarryOn
 {
