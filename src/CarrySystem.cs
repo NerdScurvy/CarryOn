dfae46fa4e819a5c5874f18d564c32a94a1a7347
NerdScurvy <NerdScurvy@gmail.com>
2025-08-28 18:26:40 +0800

    Bump version to 1.10.1

--- src/CarrySystem.cs
+++ src/CarrySystem.cs
@@ -21,7 +21,7 @@ using Vintagestory.GameContent;
 
 [assembly: ModInfo("Carry On",
     modID: "carryon",
-    Version = "1.10.0",
+    Version = "1.10.1",
     Description = "Adds the capability to carry various things",
     Website = "https://github.com/NerdScurvy/CarryOn",
     Authors = new[] { "copygirl", "NerdScurvy" })]
